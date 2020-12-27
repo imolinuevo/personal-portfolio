8f7f622664c042e14ed939cf2b41bdde2405687a
imolinuevo <i.m.parron@opendesuto.es>
2020-12-27 19:45:11 +0100

    add media query for items in landing block and scaffold for title container

--- src/components/landing-block/LandingBlock.tsx
+++ src/components/landing-block/LandingBlock.tsx
@@ -5,7 +5,10 @@ import "./LandingBlock.css";
 const LandingBlock = () => {
   return (
     <div className="main-block landing-block">
-      <MiniBrowser className='mini-browser'/>
+      <MiniBrowser className="mini-browser" />
+      <div className="title-container">
+        <h1>Foo</h1>
+      </div>
     </div>
   );
 };
