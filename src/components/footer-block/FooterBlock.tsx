142aa77f20325b30c21100ea7bdf94a1e6d8cbc3
imolinuevo <i.m.parron@opendesuto.es>
2020-12-29 19:17:24 +0100

    finish styleing footer

--- src/components/footer-block/FooterBlock.tsx
+++ src/components/footer-block/FooterBlock.tsx
@@ -6,14 +6,16 @@ import LinkedinIcon from "../social-icons/LinkedinIcon";
 const FooterBlock = () => {
   return (
     <div className="main-block footer-block">
-      <h1>Contact me</h1>
-      <p>
-        If you liked my portfolio consider contacting me for further
-        professional information through my social networks.
-      </p>
-      <div className="social-footer">
-        <GithubIcon />
-        <LinkedinIcon />
+      <div className="footer-content">
+        <h1>Contact me</h1>
+        <p>
+          If you liked my portfolio consider contacting me for further
+          professional information through my social networks.
+        </p>
+        <div className="social-footer">
+          <GithubIcon />
+          <LinkedinIcon />
+        </div>
       </div>
     </div>
   );
