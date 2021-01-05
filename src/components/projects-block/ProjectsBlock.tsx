e58864a805dd25e5d197f3c0e1f9dea481530f48
imolinuevo <i.m.parron@opendesuto.es>
2021-01-05 12:33:31 +0100

    add initial project list and state

--- src/components/projects-block/ProjectsBlock.tsx
+++ src/components/projects-block/ProjectsBlock.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProjectsBlock.css";
 import { ReactComponent as LeftNavigationIcon } from "./navigation-icons/left-navigation-icon.svg";
 import { ReactComponent as RightNavigationIcon } from "./navigation-icons/right-navigation-icon.svg";
+import projectList from "./projectList";
 
 const ProjectsBlock = () => {
+  const [projects, setProjects] = useState(projectList);
+  const [activeProject, setActiveProject] = useState(projects[0]);
+
   return (
     <div className="main-block projects-block" id="projects">
       <div className="block-title">
@@ -12,27 +16,29 @@ const ProjectsBlock = () => {
       <div className="carousel">
         <div className="project-container">
           <div className="image-container">
-            <img src="" alt="" />
+            <img
+              src={
+                require(`./snapshots/${activeProject.title.toLocaleLowerCase()}.png`)
+                  .default
+              }
+              alt={activeProject.title}
+            />
           </div>
           <div className="project-description-container">
             <div className="left-nav">
               <LeftNavigationIcon />
             </div>
             <div className="text-container">
-              <h4 className="project-description-title">Title</h4>
-              <p className="project-description-text">Project description.</p>
+              <h4 className="project-description-title">
+                {activeProject.title}
+              </h4>
+              <p className="project-description-text">
+                {activeProject.description}
+              </p>
               <p className="project-description-tags">
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
-                <span>tag</span>
+                {activeProject.tags.map((tag) => (
+                  <span key={tag}>{tag}</span>
+                ))}
               </p>
             </div>
             <div className="right-nav">
---
+++ src/components/projects-block/projectList.tsx
@@ -0,0 +1,16 @@
+import { IProject } from "./IProject";
+
+const projectList: IProject[] = [
+  {
+    title: "foo",
+    description: "bar",
+    tags: ["tag1", "tag2"],
+  },
+  {
+    title: "bar",
+    description: "foo",
+    tags: ["tag1", "tag2", "tag3"],
+  },
+];
+
+export default projectList;
